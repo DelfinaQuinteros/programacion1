c40f6fea66ccb61ae9776260e73bf696aee00398
DelfinaQuinteros <65193862+DelfinaQuinteros@users.noreply.github.com>
2021-03-23 14:48:09 -0300

    Update install.sh
    # arreglo de errores

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 python3 -m venv . # Crear el entorno virtual
 source bin/activate #activar el entrono
-pip3 install -r requirments.txt # instala las cosas
+pip3 install -r requirements.txt # instala las cosas
