84a3fab1b857300b6593dc2a6e7ac03813e5544f
delfina quinteros <dquinteros4297@gmail.com>
2021-09-21 13:43:28 -0300

    avances

--- frontend/main/routes/inicio.py
+++ frontend/main/routes/inicio.py
@@ -10,7 +10,19 @@ def index():
     return render_template('inicio.html')
 
 
-@inicio.route('/view/<int:id>')
-def view(id):
+@inicio.route('/iniciar_sesion/<int:id>')
+def inicio_sesion(id):
     # Mostrar template
-    return render_template('inicio_registrado.html')
+    return render_template('inicio_sesion.html')
+
+
+@inicio.route('/registrarse/<int:id>')
+def registrarse(id):
+    # Mostrar template
+    return render_template('registrarse.html')
+
+
+@inicio.route('/ver-bolsones/<int:id>')
+def ver_bolsones(id):
+    # Mostrar template
+    return render_template('bolsones_no_logeado.html')
