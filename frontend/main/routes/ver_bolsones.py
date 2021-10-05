4698fd4de103ddb82dc7d54bfad957916d09527e
DelfinaQuinteros <65193862+DelfinaQuinteros@users.noreply.github.com>
2021-10-05 18:11:07 -0300

    05/10

--- frontend/main/routes/ver_bolsones.py
+++ frontend/main/routes/ver_bolsones.py
@@ -1,18 +1,32 @@
 from flask import Blueprint, render_template
+import requests, json
 
 ver_bolsones = Blueprint('ver_bolsones', __name__, url_prefix='/ver_bolsones')
 
 
 @ver_bolsones.route('/')
 def index():
-    return render_template('bolsones_no_logeado.html')
+    data = {}
+    data['page'] = 1
+    # Generar consulta GET al endpoint
+    r = requests.get(
+        current_app.config["API_URL"]+'/ver_bolsones',
+        headers={"content-type":"application/json"},
+        data = json.dumps(data))
+    #Convertir respuesta de JSON a  diccionario
+    print(r)
+    bolsones_no_log = json.loads(r.text)["bolsones"]
+    #Mostrar template
+    return render_template('bolsones_no_logeado.html', bolsones_no_log=bolsones_no_log)
 
 
 @ver_bolsones.route('/inicio_sesion/')
 def inicio_sesion():
+    """usar create y poner los datos de inicio de sesion con el form"""
     return render_template('inicio_sesion.html')
 
 
 @ver_bolsones.route('/registrarse/')
 def registrarse():
+    """usar create y poner los datos de registrarse con el form"""
     return render_template('registrarse.html')
